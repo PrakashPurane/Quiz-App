96e58c35735774f0f193f35b5125ac861605b42f
PrakashPurane <chhanprakash@gmail.com>
2024-05-12 22:52:33 -0700

    darkmode added

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Quiz from "./assets/components/Quiz/Quiz";
 import Mode from "./assets/components/Mode/Mode";
-// import './App.css'
+ import './App.css'
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <>
-      <Mode />
-      <Quiz />
+    <div className={`app-container  ${isDarkMode ? 'dark-mode' : ''}`}>
+
+      <Mode toggleDarkMode={toggleDarkMode} />
+      <Quiz isDarkMode={isDarkMode} />
+    </div>
     </>
   );
 };
